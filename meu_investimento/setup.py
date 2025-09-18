1d2063b38c60b491c1fcc46b88babc3f423b9ee5
Imperat0 <imperato06@gmail.com>
2025-09-18 18:50:46 -0300

    Python

---
+++ meu_investimento/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="meu_investimento",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Daniel Imperato",
+    author_email="imperato06@gmail.com",
+    description="Uma biblioteca para cálculos de investimentos.",
+    url="https://github.com/Imperat0/meu-investimento",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
