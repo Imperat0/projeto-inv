3473c85c9e806205b73bae79c840c9e81f0fda51
Imperat0 <imperato06@gmail.com>
2025-09-18 19:53:56 -0300

    versao nova

--- meu_investimento/setup.py
+++ meu_investimento/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="meu_investimento",
-    version="0.2",
+    version="0.4",
     packages=find_packages(),
     install_requires=[],
     author="Daniel Imperato",
